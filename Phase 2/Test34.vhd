5f276840657824c74117b9cb993abb9400462223
AhmedWael02 <ahmedwael2062@gmail.com>
2022-05-12 19:51:44 +0300

    Testing Done, Report Done, Project Completed

---
+++ Phase 2/Test34.vhd
@@ -0,0 +1,100 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   16:09:31 05/12/2022
+-- Design Name:   
+-- Module Name:   C:/Users/medom/OneDrive/Desktop/Try3/Try11/Test34.vhd
+-- Project Name:  Try1
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: ALU
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY Test34 IS
+END Test34;
+ 
+ARCHITECTURE behavior OF Test34 IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT ALU
+    PORT(
+         data1 : IN  std_logic_vector(31 downto 0);
+         data2 : IN  std_logic_vector(31 downto 0);
+         aluop : IN  std_logic_vector(3 downto 0);
+         cin : IN  std_logic;
+         dataout : OUT  std_logic_vector(31 downto 0);
+         cflag : OUT  std_logic;
+         zflag : OUT  std_logic;
+         oflag : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal data1 : std_logic_vector(31 downto 0) := (others => '0');
+   signal data2 : std_logic_vector(31 downto 0) := (others => '0');
+   signal aluop : std_logic_vector(3 downto 0) := (others => '0');
+   signal cin : std_logic := '0';
+
+ 	--Outputs
+   signal dataout : std_logic_vector(31 downto 0);
+   signal cflag : std_logic;
+   signal zflag : std_logic;
+   signal oflag : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: ALU PORT MAP (
+          data1 => data1,
+          data2 => data2,
+          aluop => aluop,
+          cin => cin,
+          dataout => dataout,
+          cflag => cflag,
+          zflag => zflag,
+          oflag => oflag
+        );
+
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+     
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
---
+++ Phase 2/test793.vhd
@@ -0,0 +1,79 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+
+ 
+ENTITY test793 IS
+END test793;
+ 
+ARCHITECTURE behavior OF test793 IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT MIPSCPU
+    PORT(
+         START : IN  std_logic;
+         CLK : IN  std_logic;
+         RegFileOut1 : OUT  std_logic_vector(31 downto 0);
+         RegFileOut2 : OUT  std_logic_vector(31 downto 0);
+         ALUOut : OUT  std_logic_vector(31 downto 0);
+         PCOut : OUT  std_logic_vector(31 downto 0);
+         DataMemOut : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+   --Inputs
+   signal START : std_logic := '0';
+   signal CLK : std_logic := '0';
+
+ 	--Outputs
+   signal RegFileOut1 : std_logic_vector(31 downto 0);
+   signal RegFileOut2 : std_logic_vector(31 downto 0);
+   signal ALUOut : std_logic_vector(31 downto 0);
+   signal PCOut : std_logic_vector(31 downto 0);
+   signal DataMemOut : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant CLK_period : time := 10 ns;
+
+	
+BEGIN
+
+  --alias some_buried_sig is << signal uut.MemRead : std_logic >>;
+
+	--MemRead <= <<signal test793.uut:.MemRead : std_logic>>; 
+	
+	-- Instantiate the Unit Under Test (UUT)
+   uut: MIPSCPU PORT MAP (
+          START => START,
+          CLK => CLK,
+          RegFileOut1 => RegFileOut1,
+          RegFileOut2 => RegFileOut2,
+          ALUOut => ALUOut,
+          PCOut => PCOut,
+          DataMemOut => DataMemOut
+        );
+
+   -- Clock process definitions
+   CLK_process :process
+   begin
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+START <= '1';
+WAIT for 30 ns;
+START <= '0';
+WAIT for 30ns;
+
+
+      wait;
+   end process;
+
+END;
